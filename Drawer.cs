46f81465d6b63621cd58a5130c47c977957d2221
Sergey <serridd@gmail.com>
2020-05-21 21:41:06 +0300

    Command input/Chatbox.

--- Drawer.cs
+++ Drawer.cs
@@ -2,6 +2,7 @@ using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 
+using System.Linq;
 namespace rotstein
 {
     class Drawer
@@ -10,9 +11,14 @@ namespace rotstein
         static readonly int SCALE = 6; // Game scale
         static readonly int TEXTURE_SIZE = 16;
 
-        Vector2u WindowSize = new Vector2u(1600, 900);
+        Vector2u WindowSize = new Vector2u(1600, 900); // TODO: change type to Vector2f and remove `* SCALE` everywhere
         Texture Atlas;
         RenderWindow Window;
+        InputState InpState = InputState.None;
+        string Chatbox = "";
+        Font BasicFont = new Font("SourceCodePro-Regular.otf");
+        // Font BasicFont = new Font("Cantarell-Regular.otf");
+
         Game Game;
 
         public Drawer()
@@ -32,39 +38,92 @@ namespace rotstein
 
             Window.KeyPressed += (_, args) =>
             {
-                View view = Window.GetView();
-                int shift = TEXTURE_SIZE;
-                switch (args.Code)
+                switch (InpState)
                 {
-                    case Keyboard.Key.W:
-                        Game.Player.Position.Y -= (uint)shift;
-                        Game.Player.NextAnimationStep();
-                        view.Move(new Vector2f(0f, -shift));
+                    case InputState.None:
+                        switch (args.Code)
+                        {
+                            case Keyboard.Key.Tilde:
+                                InpState = InputState.Chat;
+                                break;
+                            default:
+                                View view = Window.GetView();
+                                int shift = TEXTURE_SIZE;
+                                switch (args.Code)
+                                {
+                                    case Keyboard.Key.W:
+                                        Game.Player.Position.Y -= (uint)shift;
+                                        Game.Player.NextAnimationStep();
+                                        view.Move(new Vector2f(0f, -shift));
+                                        break;
+                                    case Keyboard.Key.A:
+                                        Game.Player.Direction = false;
+                                        Game.Player.NextAnimationStep();
+                                        Game.Player.Position.X -= (uint)shift;
+                                        view.Move(new Vector2f(-shift, 0f));
+                                        break;
+                                    case Keyboard.Key.S:
+                                        Game.Player.Position.Y += (uint)shift;
+                                        Game.Player.NextAnimationStep();
+                                        view.Move(new Vector2f(0f, shift));
+                                        break;
+                                    case Keyboard.Key.D:
+                                        Game.Player.Direction = true;
+                                        Game.Player.NextAnimationStep();
+                                        Game.Player.Position.X += (uint)shift;
+                                        view.Move(new Vector2f(shift, 0f));
+                                        break;
+                                }
+                                Window.SetView(view);
+                                break;
+                        }
                         break;
-                    case Keyboard.Key.A:
-                        Game.Player.Direction = false;
-                        Game.Player.NextAnimationStep();
-                        Game.Player.Position.X -= (uint)shift;
-                        view.Move(new Vector2f(-shift, 0f));
+                    case InputState.Chat:
+                        switch (args.Code)
+                        {
+                            case Keyboard.Key.Tilde:
+                                InpState = InputState.None;
+                                break;
+                            case Keyboard.Key.Backspace:
+                                if (Chatbox.Length > 0)
+                                {
+                                    Chatbox = Chatbox.Remove(Chatbox.Length - 1);
+                                }
+                                break;
+                            case Keyboard.Key.Enter:
+                                Chatbox = "";
+                                // TODO: execute command
+                                InpState = InputState.None;
+                                break;
+                        }
+                        // Chatbox input is handled in TextEntered event
                         break;
-                    case Keyboard.Key.S:
-                        Game.Player.Position.Y += (uint)shift;
-                        Game.Player.NextAnimationStep();
-                        view.Move(new Vector2f(0f, shift));
-                        break;
-                    case Keyboard.Key.D:
-                        Game.Player.Direction = true;
-                        Game.Player.NextAnimationStep();
-                        Game.Player.Position.X += (uint)shift;
-                        view.Move(new Vector2f(shift, 0f));
-                        break;
+                    default:
+                        throw new System.Exception("Unhandled input state.");
                 }
-                Window.SetView(view);
+            };
+
+            Window.TextEntered += (_, args) =>
+            {
+                switch (InpState)
+                {
+                    case InputState.None:
+                        // Game input is handled in KeyPressed event
+                        break;
+                    case InputState.Chat:
+                        if (args.Unicode.Any(c => char.IsControl(c) | "`~".Contains(c)))
+                        {
+                            break;
+                        }
+                        Chatbox += args.Unicode;
+                        break;
+                    default:
+                        throw new System.Exception("Unhandled input state.");
+                }
             };
 
             Window.MouseButtonPressed += (_, args) =>
             {
-                //System.Console.WriteLine(args);
                 Vector2u tile_coord = new Vector2u(
                     (uint)System.Math.Ceiling((float)((args.X - WindowSize.X / 2) / SCALE + Game.Player.Position.X) / (float)(TEXTURE_SIZE) - 0.5),
                     (uint)System.Math.Ceiling((float)((args.Y - WindowSize.Y / 2) / SCALE + Game.Player.Position.Y) / (float)(TEXTURE_SIZE)));
@@ -102,6 +161,8 @@ namespace rotstein
                     }
                 }
                 DrawPlayer();
+                DrawGui();
+
                 Window.Display();
                 System.Threading.Thread.Sleep(15);
             }
@@ -134,6 +195,36 @@ namespace rotstein
             sprite.Position = new Vector2f(Game.Player.Position.X, Game.Player.Position.Y);
 
             Window.Draw(sprite);
+        }
+
+        void DrawGui()
+        {
+            Vector2f center = Window.GetView().Center;
+            // Coordinates of Window 0,0 pixel in **the world**.
+            // Required to trick view to always display gui regardless of view position.
+            Vector2f window_zero = new Vector2f(
+                center.X - WindowSize.X / SCALE / 2,
+                center.Y - WindowSize.Y / SCALE / 2);
+
+            if (InpState == InputState.Chat)
+            {
+                RectangleShape chat_background = new RectangleShape(new Vector2f(100, 10));
+                chat_background.Position = window_zero + new Vector2f(0, WindowSize.Y / SCALE / 4 * 3);
+                chat_background.FillColor = new Color(100, 100, 100, 200);
+                Window.Draw(chat_background);
+
+                Text text = new Text('>' + Chatbox + (System.DateTime.Now.Second % 2 == 0 ? "" : "|"), BasicFont, 60);
+                text.Scale = new Vector2f(0.14f, 0.14f);
+                text.FillColor = new Color(0, 0, 0);
+                text.Position = chat_background.Position + new Vector2f(1.0f, -0.8f);
+                Window.Draw(text);
+            }
+        }
+
+        enum InputState
+        {
+            None,
+            Chat,
         }
     }
 }
